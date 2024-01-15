3d92254d38e9f2e5b81dc0278ebb2864e7cbd24f
Sajjad Esmaeeli <s.sajjad.esmaeeli@gmail.com>
2024-01-15 21:18:41 +0100

    add transaction for creation of log and actual inc/dec

--- src/Traits/BalanceOperation.php
+++ src/Traits/BalanceOperation.php
@@ -3,6 +3,7 @@
 namespace HPWebdeveloper\LaravelPayPocket\Traits;
 
 use HPWebdeveloper\LaravelPayPocket\Models\WalletsLog;
+use Illuminate\Support\Facades\DB;
 use InvalidArgumentException;
 
 trait BalanceOperation
@@ -22,8 +23,10 @@ trait BalanceOperation
      */
     public function decrementAndCreateLog(int|float $value, ?string $notes = null): void
     {
-        $this->createLog('dec', $value, $notes);
-        $this->decrement('balance', $value);
+        DB::transaction(function () use ($value, $notes) {
+            $this->createLog('dec', $value, $notes);
+            $this->decrement('balance', $value);
+        });
     }
 
     /**
@@ -31,8 +34,10 @@ trait BalanceOperation
      */
     public function incrementAndCreateLog(int|float $value, ?string $notes = null): void
     {
-        $this->createLog('inc', $value, $notes);
-        $this->increment('balance', $value);
+        DB::transaction(function () use ($value, $notes) {
+            $this->createLog('inc', $value, $notes);
+            $this->increment('balance', $value);
+        });
     }
 
     /**
