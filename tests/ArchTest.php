ae280ceae0701c9dbf797fac5c34f05ea13a1b3a
Legacy <52163001+3m1n3nc3@users.noreply.github.com>
2025-06-26 02:58:08 +0100

    feat: Write test for the TransactionCompleted event.

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,24 @@
 <?php
 
+use Illuminate\Support\Facades\Event;
+use ToneflixCode\LaravelPayPocket\Events\TransactionCompleted;
+
 it('will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
+
+ 
+test('dispatches events', function () {
+    Event::fake();
+ 
+    $user = User::factory()->create();
+    
+    // Perform wallet update...
+    $type = 'main_escrow';
+    $user->deposit($type, 234.56);
+ 
+    // Assert that an event was dispatched...
+    Event::assertDispatched(function (TransactionCompleted $event) {
+        return $event->type === 'inc';
+    });
+});
